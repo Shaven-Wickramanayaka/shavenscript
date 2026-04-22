467ec893575c19738898ce9d8d3b627ab6143097
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-04-22 11:31:33 +0100

    Added guide lalrpop file

--- src/ast.rs
+++ src/ast.rs
@@ -1,1 +1,30 @@
-#derive
+#[derive(Clone)]
+pub enum Expr{
+    Number(f64),
+    StringLiteral(String),
+    Bool(bool),
+    Variable(String),
+    BinOp(Box<Expr>, Op, Box<Expr>),
+    Call(String, Vec<Expr>)
+}
+#[derive(Clone)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Eq,
+    Lt,
+    Gt,
+}
+
+#[derive(Clone)]
+pub enum Statement {
+    Puts(Expr),
+    Assign(String, Expr),
+    If(Expr, Vec<Statement>, Option<Vec<Statement>>),
+    While(Expr, Vec<Statement>),
+    Def(String, Vec<String>, Vec<Statement>),
+    Return(Expr),
+    ExprStatement(Expr),
+}
