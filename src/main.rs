2f9e94f561f5f483cd0121af48bb69b9a8d92ae2
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-04-24 22:29:34 +0100

    Added source file reading and changed print syntax

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,7 @@ use interpreter::Interpreter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_name = args.get(1);
-    let mut source_file = match File::open(file_name){
+    let mut source_file = match File::open(args[1].clone()){
         Ok(source_file) => source_file,
         Err(err) => panic!("File Could not be opened: {}", err),
     };
@@ -26,43 +25,8 @@ fn main() {
         Ok(source) => source,
         Err(err) => panic!("Instructions could not be read: {}", err),
     };
-    dbg!(args);
-    let source = r#"
-def greet(name)
-    puts "Hello, " + name + "!";
-end;
-
-greet("world");
-
-x = 10;
-y = 3;
-puts x + y;
-
-def factorial(n)
-    if n < 2 then
-        return 1;
-    end;
-    return n * factorial(n - 1);
-end;
-
-result = factorial(5);
-puts result;
-
-i = 1;
-while i < 6 do
-    puts i;
-    i = i + 1;
-end;
-
-if 10 > 5 then
-    puts "ten is greater";
-else
-    puts "this won't print";
-end;
-"#;
-
     let parser = shavenscript::ProgramParser::new();
-    let program = parser.parse(source).unwrap_or_else(|e| {
+    let program = parser.parse(source.as_str()).unwrap_or_else(|e| {
         eprintln!("Parse error: {e}");
         std::process::exit(1);
     });
